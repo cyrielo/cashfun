d8960decc2caba010969e7a2d9a0ccebfb96fc1e
Cyril Ologho <cyril.paul@andela.com>
2017-07-18 22:29:52 +0100

    Update signup component with content

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Form from './../partials/form';
+import authStyle from './../../styles/auth.scss';
 
 class SignUp extends React.Component {
 	render() {
 		return (
-			<p>
-				SignUp Here
-			</p>
+			<div className="container">
+				<div className="row auth-row">
+
+					<div className="col s12 m6 offset-m3 l5 offset-l3" id="auth-page">
+						<div id="auth-container">
+							<div id="auth-brand">
+								<div id="brand-details">
+									<div id="brand-color">
+										<div id="initials">cf</div>
+									</div>
+									<div id="site-name">cashfun</div>
+								</div>
+
+								<div id="connections">
+									<img id="first-image" src="/dist/img/img1.jpg" />
+									<img id="second-image" src="/dist/img/img3.jpg" />
+									<img id="third-image" src="/dist/img/img2.jpg" />
+								</div>
+							</div>
+							<Form type='signup' />
+						</div>
+					</div>
+					<div className="col s12 m6 offset-m3 l5 offset-l3 white center-align" id="auth-footer">
+						Already have an Account? <Link className="blue-text" to="/signin">Signin</Link>
+					</div>
+
+				</div>
+			</div>
 		);
 	}
 }
 
 
 export default SignUp;
+
